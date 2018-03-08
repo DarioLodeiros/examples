da28c641691b3e0aa6a2a54e500614abf0cdf266
Dario Lodeiros <dario@aloxa.eu>
2018-03-08 12:11:34 +0100

    First commit

---
+++ test_library/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Library',
+    'version': '1.0.1.0.0',
+    'summary': "Library Manager.",
+    'description': '',
+    'category': '',
+    'author':
+        'Dario',
+    'website': '',
+    'license': 'AGPL-3',
+    'depends': [
+        'base'
+    ],
+    'data': [
+    ],
+    'qweb': [],
+    'installable': True,
+}
